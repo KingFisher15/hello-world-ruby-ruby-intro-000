3448e6e502be5c01adbe56fa393c1673589a4b86
crisp-delimiter-5393 <gordond227@gmail.com>
2020-07-05 22:56:25 +0000

    Automatically backed up by Learn

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -1,4 +1,3 @@
-touch hello_world rb.
 puts "Hello world"
   it 'has a file hello_world.rb' do
     expect(File.exists?("hello_world.rb")).to be_truthy, "Did you create the file hello_world.rb? (If so, is it spelled exactly the same with the same capitalization?)"
